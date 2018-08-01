6e358c320c2924ebc6af53c9873fd9b4d5cae3a1
Chance Eakin <chance.eakin@gmail.com>
2018-08-01 08:13:34 -0500

    fixed a few things

--- profile-react/src/components/HomePage/HomePage.js
+++ profile-react/src/components/HomePage/HomePage.js
@@ -1,22 +1,26 @@
 import React, { Component } from "react";
-import { Container, Col, Row } from "reactstrap"
-import './HomePage.css'
+import { Container, Col, Row } from "reactstrap";
+import "./HomePage.css";
 
 export default class HomePage extends Component {
   render() {
     return (
-    <Container>
+      <Container className="overlay">
         <Row>
-            <Col lg-12>
-                <div id="content">
-                <h1 className="title">Jose Ayala</h1>
-                <h3 className="portfolio">A Portfolio</h3>
-                <hr className="hr"/>
-                <h3><a className="enter" href = "projects.html">Enter</a></h3>
-                </div>
-            </Col>
+          <Col lg-12>
+            <div className="content">
+              <h1 className="title">Jose Ayala</h1>
+              <h3 className="portfolio">A Portfolio</h3>
+              <hr className="hr" />
+              <h3>
+                <a className="enter" href="projects.html">
+                  Enter
+                </a>
+              </h3>
+            </div>
+          </Col>
         </Row>
-    </Container>
+      </Container>
     );
   }
 }
